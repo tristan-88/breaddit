08794be8a238a6724a607bddf15faf5600996775
John-Michael Riley <johnmichael.w.riley@gmail.com>
2021-02-17 17:45:58 -0500

    add appropriate associations

--- models/user.js
+++ models/user.js
@@ -1,13 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar_url: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
+  const User = sequelize.define(
+    'User',
+    {
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar_url: DataTypes.STRING,
+    },
+    {}
+  );
+  User.associate = function (models) {
     // associations can be defined here
+    User.hasMany(models.Post, { foreignKey: 'userId' });
   };
   return User;
 };
