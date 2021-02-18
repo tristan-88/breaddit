5503de7fd419a43d8eb2a7249d5cf640e44358e6
John-Michael Riley <johnmichael.w.riley@gmail.com>
2021-02-18 11:37:24 -0500

    add update examples

---
+++ updating.js
@@ -0,0 +1,16 @@
+const { User, sequelize } = require('./models/index');
+
+const updateEmail = async (newEmail) => {
+  // update property directly, then call .save
+  // const user = await User.findByPk(3);
+  // console.log(user.toJSON());
+  // user.email = newEmail;
+  // await user.save();
+  // call the update class method
+  // await User.update({ email: newEmail }, { where: { username: 'justLoafin' } });
+  // update instance method
+  const user = await User.findByPk(3);
+  await user.update({ email: newEmail, username: 'justLance' });
+};
+
+updateEmail('lancebakesbread@hotmail.com');
